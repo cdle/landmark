ebd6dccc818b5b30f1f398ac4a96146b9c243855
aiancandle <798731886@qq.com>
2020-01-08 10:21:02 +0800

    Initial Commit

---
+++ landmark/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  landmark
+//
+//  Created by 邰胜广 on 2020/1/8.
+//  Copyright © 2020 邰胜广. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
